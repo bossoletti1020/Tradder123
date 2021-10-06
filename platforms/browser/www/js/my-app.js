47f2cde08c67dbc68d022150bf9d2f240d7d8039
Jero <jerobeatle@gmail.com>
2021-10-06 17:00:20 -0300

    Commit Desarrollo

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,50 +1,44 @@
-  // If we need to use custom DOM library, let's save it to $$ variable:
-  var $$ = Dom7;
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
 
-  var app = new Framework7({
-      // App root element
-      root: '#app',
-      // App Name
-      name: 'My App',
-      // App id
-      id: 'com.myapp.test',
-      // Enable swipe panel
-      panel: {
-          swipe: 'left',
-      },
-      // Add default routes
-      routes: [{
-                  path: '/about/',
-                  url: 'about.html',
-              },
-              {
-                  path: '/Tradder/',
-                  url: 'tradder.html',
-              },
-              {
-                  path: '/index/',
-                  url: 'index.html',
-              },
-          ]
-          // ... other parameters
-  });
+var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+        swipe: 'left',
+    },
+    // Add default routes
+    routes: [
+        {path: '/about/', url: 'about.html',},
 
-  var mainView = app.views.create('.view-main');
+        {path: '/Tradder/',url: 'tradder.html',},
+        
+        {path: '/index/', url: 'index.html',},
+    ]
+    // ... other parameters
+});
 
-  // Handle Cordova Device Ready Event
-  $$(document).on('deviceready', function() {
-      console.log("Device is ready!");
-  });
+var mainView = app.views.create('.view-main');
 
-  // Option 1. Using one 'page:init' handler for all pages
-  $$(document).on('page:init', function(e) {
-      // Do something here when page loaded and initialized
-      console.log(e);
-  })
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function () {
+    console.log("Device is ready!");
+});
 
-  // Option 2. Using live 'page:init' event handlers for each page
-  $$(document).on('page:init', '.page[data-name="about"]', function(e) {
-      // Do something here when page with data-name="about" attribute loaded and initialized
-      console.log(e);
-      alert('Hello');
-  })
+// Option 1. Using one 'page:init' handler for all pages
+$$(document).on('page:init', function (e) {
+    // Do something here when page loaded and initialized
+    console.log(e);
+})
+
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+    alert('Hello');
+})
