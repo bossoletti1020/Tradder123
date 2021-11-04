4cd0cfa6d96038c3a286fe891b6f50141f3c2857
Jero <jerobeatle@gmail.com>
2021-11-04 18:49:51 -0300

    Commit Registro y Login funcional

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,15 +14,15 @@ var app = new Framework7({
     },
     // Add default routes
     routes: [
-        { path: '/about/', url: 'about.html', },
 
         { path: '/Tradder/', url: 'tradder.html', },
 
         { path: '/index/', url: 'index.html', },
+        
+        { path: '/registro/', url: 'registrar.html', },
+        
+        { path: '/inicio/', url: 'iniciar.html', },
 
-        { path: '/iniciar/', url: 'iniciar.html', },
-
-        { path: '/registra/', url: 'registrar.html', },
     ]
     // ... other parameters
 });
@@ -40,104 +40,89 @@ $$(document).on('page:init', function (e) {
     console.log(e);
 })
 
-// Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
-    // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
-})
-
-// $$(document).on('page:init', '.page[data-name="iniciar"]', function (e) {
-//     // Do something here when page with data-name="about" attribute loaded and initialized
-//     console.log(e);
-
-//     $$('#lGo').on('click', fnLogin);
-
-// })
-
-$$(document).on('page:init', '.page[data-name="registra"]', function (e) {
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
     console.log(e);
 
-    $$('#rGo').on('click', fnRegistro);
+})
+
+$$(document).on('page:init', '.page[data-name="registro"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+
+    $$('#rRegistro').on('click', fnRegistro);
 
 })
 
-// Sacar la navbar en el Iniciar sesión
-// $$(document).on('page:init', '.page[data-name="iniciar"]', function (e) {
-//     $$(".navbar").hide();
-// })
+$$(document).on('page:init', '.page[data-name="inicio"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
 
-// $$(document).on('page:init', '.page[data-name="index"]', function (e) {
-//     $$(".navbar").show();
-// })
+    $$('#lLogin').on('click', fnLogin);
 
+})
 
-
-
-
-var emailDelUser = "";
+var emailDelUser="";
 
 var db = firebase.firestore();
 var colUsuario = db.collection("Usuarios");
 
-//Loguear
+//Logueo
 
-// function fnLogin() {
+function fnLogin() {
 
-//     emailDelUser = $$('#lEmail').val();
-//     passDelUser = $$('#lPass').val();
+    emailDelUser = $$('#lEmail').val();
+    passDelUser = $$('#lPass').val();
 
-//     firebase.auth().signInWithEmailAndPassword(emailDelUser, passDelUser)
-//         .then((userCredential) => {
-//             // Signed in
-//             var user = userCredential.user;
+    firebase.auth().signInWithEmailAndPassword(emailDelUser, passDelUser)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
 
-//             console.log("Bienvenid@!!! " + emailDelUser);
+        console.log("Bienvenid@!!! " + emailDelUser);
 
-//             claveDeColeccion = emailDelUser;
+        claveDeColeccion = emailDelUser;
 
-//             var docRef = colUsuario.doc(claveDeColeccion);
+        var docRef = colUsuario.doc(claveDeColeccion);
 
-//             docRef.get().then((doc) => {
-//                 if (doc.exists) {
-//                     console.log("Document data:", doc.data());
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
 
-//                     console.log(doc.id);
-//                     console.log(doc.data().nombre);
-//                     console.log(doc.data().rol);
+                console.log(doc.id);
+                console.log(doc.data().nombre);
+                console.log(doc.data().rol);
 
-//                     if (doc.data().rol == "admin") {
-//                         mainView.router.navigate('/panelAdmin/');
-//                     } else {
-//                         mainView.router.navigate('/panelUsuario/');
-//                     }
-
-
-//                 } else {
-//                     // doc.data() will be undefined in this case
-//                     console.log("No such document!");
-//                 }
-//             }).catch((error) => {
-//                 console.log("Error getting document:", error);
-//             });
+                if (doc.data().rol == "admin") {
+                    mainView.router.navigate('/panelAdmin/');
+                } else {
+                    mainView.router.navigate('/Tradder/');
+                }
 
 
-//             // ...
-//         })
-//         .catch((error) => {
-//             var errorCode = error.code;
-//             var errorMessage = error.message;
-
-//             console.error(errorCode);
-//             console.error(errorMessage);
-//         });
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
 
 
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.error(errorCode);
+            console.error(errorMessage);
+      });
 
 
-// }
 
+
+}
 
 //Registro
 
@@ -147,13 +132,14 @@ function fnRegistro() {
     // cada un@ pone su magia para recuperar el mail y la clave de un form...
     emailDelUser = $$('#rEmail').val();
     passDelUser = $$('#rPass').val();
-
+    
 
     firebase.auth().createUserWithEmailAndPassword(emailDelUser, passDelUser)
         .then((userCredential) => {
             // Signed in
             var user = userCredential.user;     
             console.log("Bienvenid@!!! " + emailDelUser);
+            console.log(passDelUser);
             // ...
             //mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
 
@@ -161,7 +147,7 @@ function fnRegistro() {
             claveDeColeccion = emailDelUser;
 
             nombre = $$('#rNombre').val();
-
+           
             datos = {
                 nombre: nombre,
                 rol: "usuario"
