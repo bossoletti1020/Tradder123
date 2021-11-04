14bb47f3cb54100c3639ef65d9f7ad40f0e779a4
Jero <jerobeatle@gmail.com>
2021-11-03 22:16:25 -0300

    Commit Registro

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,13 +14,15 @@ var app = new Framework7({
     },
     // Add default routes
     routes: [
-        {path: '/about/', url: 'about.html',},
+        { path: '/about/', url: 'about.html', },
 
-        {path: '/Tradder/',url: 'tradder.html',},
-        
-        {path: '/index/', url: 'index.html',},
+        { path: '/Tradder/', url: 'tradder.html', },
 
-        {path: '/iniciar/', url: 'iniciar.html',},
+        { path: '/index/', url: 'index.html', },
+
+        { path: '/iniciar/', url: 'iniciar.html', },
+
+        { path: '/registra/', url: 'registrar.html', },
     ]
     // ... other parameters
 });
@@ -45,14 +47,152 @@ $$(document).on('page:init', '.page[data-name="about"]', function (e) {
     alert('Hello');
 })
 
+// $$(document).on('page:init', '.page[data-name="iniciar"]', function (e) {
+//     // Do something here when page with data-name="about" attribute loaded and initialized
+//     console.log(e);
+
+//     $$('#lGo').on('click', fnLogin);
+
+// })
+
+$$(document).on('page:init', '.page[data-name="registra"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+
+    $$('#rGo').on('click', fnRegistro);
+
+})
+
 // Sacar la navbar en el Iniciar sesión
-$$(document).on('page:init', '.page[data-name="iniciar"]', function (e) {
-    $$(".navbar").hide();
-})
+// $$(document).on('page:init', '.page[data-name="iniciar"]', function (e) {
+//     $$(".navbar").hide();
+// })
 
-$$(document).on('page:init', '.page[data-name="index"]', function (e) {
-    $$(".navbar").show();
-})
+// $$(document).on('page:init', '.page[data-name="index"]', function (e) {
+//     $$(".navbar").show();
+// })
 
+
+
+
+
+var emailDelUser = "";
+
+var db = firebase.firestore();
+var colUsuario = db.collection("Usuarios");
+
+//Loguear
+
+// function fnLogin() {
+
+//     emailDelUser = $$('#lEmail').val();
+//     passDelUser = $$('#lPass').val();
+
+//     firebase.auth().signInWithEmailAndPassword(emailDelUser, passDelUser)
+//         .then((userCredential) => {
+//             // Signed in
+//             var user = userCredential.user;
+
+//             console.log("Bienvenid@!!! " + emailDelUser);
+
+//             claveDeColeccion = emailDelUser;
+
+//             var docRef = colUsuario.doc(claveDeColeccion);
+
+//             docRef.get().then((doc) => {
+//                 if (doc.exists) {
+//                     console.log("Document data:", doc.data());
+
+//                     console.log(doc.id);
+//                     console.log(doc.data().nombre);
+//                     console.log(doc.data().rol);
+
+//                     if (doc.data().rol == "admin") {
+//                         mainView.router.navigate('/panelAdmin/');
+//                     } else {
+//                         mainView.router.navigate('/panelUsuario/');
+//                     }
+
+
+//                 } else {
+//                     // doc.data() will be undefined in this case
+//                     console.log("No such document!");
+//                 }
+//             }).catch((error) => {
+//                 console.log("Error getting document:", error);
+//             });
+
+
+//             // ...
+//         })
+//         .catch((error) => {
+//             var errorCode = error.code;
+//             var errorMessage = error.message;
+
+//             console.error(errorCode);
+//             console.error(errorMessage);
+//         });
+
+
+
+
+// }
+
+
+//Registro
+
+function fnRegistro() {
+
+
+    // cada un@ pone su magia para recuperar el mail y la clave de un form...
+    emailDelUser = $$('#rEmail').val();
+    passDelUser = $$('#rPass').val();
+
+
+    firebase.auth().createUserWithEmailAndPassword(emailDelUser, passDelUser)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;     
+            console.log("Bienvenid@!!! " + emailDelUser);
+            // ...
+            //mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
+
+
+            claveDeColeccion = emailDelUser;
+
+            nombre = $$('#rNombre').val();
+
+            datos = {
+                nombre: nombre,
+                rol: "usuario"
+            }
+
+            colUsuario.doc(claveDeColeccion).set(datos)
+                .then(() => {
+                    console.log("Document successfully written!");
+                })
+                .catch((error) => {
+                    console.error("Error writing document: ", error);
+                });
+
+
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.error(errorCode);
+            console.error(errorMessage);
+
+            if (errorCode == "auth/email-already-in-use") {
+                console.error("el mail ya esta usado");
+            }
+
+            // ..
+        });
+
+
+
+}
 
 
